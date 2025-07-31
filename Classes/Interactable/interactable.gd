bcc28d10dd48b06d35b28c52bc9529ef260c22ce
Mr-Patipa <rata168168@gmail.com>
2025-07-30 22:31:55 -0600

    Many Things - Check Description
     - Doors
     - ArialUI
     - Preasure Plates interaction with movable blocks (WIP)
     - Keys unlock doors

---
+++ Classes/Interactable/interactable.gd
@@ -0,0 +1,5 @@
+extends Node3D
+class_name Interactable
+
+func activate(_player : Player) -> void:
+	pass
---
+++ Scenes/Assets/Block And Preasure/preasure_plates.gd
@@ -0,0 +1,18 @@
+extends Node3D
+class_name PreasurePlates
+
+@export var Target : Interactable
+@export var Detector : Area3D
+
+
+func _ready() -> void:
+	Detector.body_entered.connect(func(body : Node3D) -> void:
+		for group in body.get_groups():
+			if group == "Block":
+				Target.activate(null)
+		)
+	Detector.body_exited.connect(func(body : Node3D) -> void:
+		for group in body.get_groups():
+			if group == "Block":
+				Target.activate(null)
+		)
