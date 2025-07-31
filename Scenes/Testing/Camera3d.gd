7e80029d73da20568b49dc823fdfca2ee1762cf5
Rwaltzwills <Rwaltzwills@gmail.com>
2025-07-31 14:17:04 +0900

    Squashed commit of the following:
    commit 86f9bd55e712be8026e472cabfd041b3a4dd5b05
    Author: Rwaltzwills <Rwaltzwills@gmail.com>
    Date:   Thu Jul 31 14:16:10 2025 +0900
    
        Changed to nearest texture rendering
    
    commit 0f8786a125b1c054d998ba93c5976864f9bf746a
    Merge: 1ced5db bcc28d1
    Author: Rwaltzwills <Rwaltzwills@gmail.com>
    Date:   Thu Jul 31 14:14:34 2025 +0900
    
        Merge branch 'main' into r-testingroom
    
    commit 1ced5dbfaf44175908b313cc4c6844dd07d139ba
    Author: Rwaltzwills <Rwaltzwills@gmail.com>
    Date:   Thu Jul 31 14:06:12 2025 +0900
    
        Outlines and a block in the testing area
    
    commit a5c2d24c129a1c02a5b815896b810c1680c9092f
    Author: Rwaltzwills <Rwaltzwills@gmail.com>
    Date:   Thu Jul 31 13:34:47 2025 +0900
    
        Camera and project settings fix so Sprite3Ds aren't blurry
    
    commit 0c7af6c149ce42e48bd0546c317af2b2f88f8a90
    Author: Rwaltzwills <Rwaltzwills@gmail.com>
    Date:   Thu Jul 31 13:19:54 2025 +0900
    
        Player lighting setup
    
    commit 3fa4bfd67ce9855231d7b3535f224585b7666dd8
    Author: Rwaltzwills <Rwaltzwills@gmail.com>
    Date:   Thu Jul 31 11:46:05 2025 +0900
    
        Test character, floor, sprite settings
    
    commit f1b1068c1c86b45fa29677f19d194013e8d3bb9a
    Author: Rwaltzwills <Rwaltzwills@gmail.com>
    Date:   Thu Jul 31 11:44:54 2025 +0900
    
        gitignore

---
+++ Scenes/Testing/Camera3d.gd
@@ -0,0 +1,11 @@
+extends Camera3D
+
+var y_lock: int
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	y_lock = self.global_position.y
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.global_position.y = y_lock
