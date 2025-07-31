88c2faadc7388bf75f0db81956b7d35959e9e198
Dmytro Shum <dinsunlight8@gmail.com>
2025-07-30 22:03:17 -0400

    Fixing PlayerGUI

--- Scenes/PlayerGUI/SessionMenu/SessionMenu.gd
+++ Scenes/PlayerGUI/SessionMenu/SessionMenu.gd
@@ -4,10 +4,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause") == true:
 		visible = !visible
 
-
-func _on_general_button_pressed() -> void:
+func _on_return_button_pressed() -> void:
 	visible = false
-
 
 func _on_quit_button_pressed() -> void:
 	get_tree().change_scene_to_file(SceneManager.MAIN_MENU)
