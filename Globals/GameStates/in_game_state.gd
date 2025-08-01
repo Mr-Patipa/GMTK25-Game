b7b7855bcf82e224cd7163b137f07c2f586d4771
Mr-Patipa <rata168168@gmail.com>
2025-08-01 13:51:51 -0600

    Villain's Dialogue Box Completed

--- Globals/GameStates/in_game_state.gd
+++ Globals/GameStates/in_game_state.gd
@@ -4,6 +4,8 @@ extends Node
 @export var DeathDuration : float = 2
 @export var DeathTimer : Timer
 @export var MusicController : AudioStreamPlayer
+@export var player : Player
+@export var SpawnPos : Marker3D
 
 @export_category("End Game Stats")
 @export var TimeCounter : Timer
@@ -12,6 +14,7 @@ var Victory : bool = false
 var IsEnding : bool = false
 
 func _ready() -> void:
+	player.global_position = SpawnPos.global_position
 	MusicController.play()
 	GameManager.time_ran_out.connect(endGamePreparation)
 	GameManager.time_change_notified.connect(playScaryMusic)
