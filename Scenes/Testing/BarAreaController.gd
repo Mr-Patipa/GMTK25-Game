874284b0cfa81993d6bc452e33431098d208aaf7
Rwaltzwills <Rwaltzwills@gmail.com>
2025-08-02 19:20:17 +0900

    Initial building of charge mechanic

--- Scenes/Testing/BarAreaController.gd
+++ Scenes/Testing/BarAreaController.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 func spawn_bartenders():
 	var right_pos = []
 	var left_pos = []
+	var bartenders = []
 	
 	right_pos.append(randf_range(0,.2))
 	right_pos.append(randf_range(.2,.4))
@@ -29,28 +30,31 @@ func spawn_bartenders():
 	
 	for p in right_pos:
 		var new_point = rightSpawns.curve.sample_baked(p*rightSpawns.curve.get_baked_length())
-		new_point = Vector3(rightSpawns.position.x, rightSpawns.position.y, new_point.z)
+		new_point = Vector3(rightSpawns.position.x, rightSpawns.position.y+1.1, new_point.z)
 
 		var new_bartender = Bartender.instantiate()
 
 		new_bartender.position = new_point
 		self.add_child(new_bartender)
-		var target = (Vector3($"Left Gutter".position.x + randi_range(-5,5),
+		new_bartender.target = (Vector3($"Left Gutter".position.x + randi_range(-5,5),
 									new_bartender.position.y,
 									new_bartender.position.z))
-		
-		# new_bartender.charge(target)
+		bartenders.append(new_bartender)
+
 		
 	for p in left_pos:
 		var new_point = leftSpawns.curve.sample_baked(p*leftSpawns.curve.get_baked_length())
-		new_point = Vector3(leftSpawns.position.x, leftSpawns.position.y, new_point.z)
+		new_point = Vector3(leftSpawns.position.x, leftSpawns.position.y+1.1, new_point.z)
 		var new_bartender = Bartender.instantiate()
 		
 		new_bartender.position = new_point
 		self.add_child(new_bartender)
 
-		var target = look_at(Vector3($"Right Gutter".position.x + randi_range(-5,5),
+		new_bartender.target = (Vector3($"Right Gutter".position.x + randi_range(-5,5),
 								new_bartender.position.y,
 								new_bartender.position.z))
+		bartenders.append(new_bartender)
 		
-		# new_bartender.charge(target)
+	await get_tree().create_timer(.33).timeout
+	for b in bartenders:
+		b.charge()
