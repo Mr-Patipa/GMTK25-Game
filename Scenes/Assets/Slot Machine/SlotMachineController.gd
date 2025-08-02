1dc67e8b8ac9fc6f94fea86d82c22fa5ab1fd786
Rwaltzwills <Rwaltzwills@gmail.com>
2025-08-02 15:31:59 +0900

    Slot Machine Memory Game

--- Scenes/Assets/Slot Machine/SlotMachineController.gd
+++ Scenes/Assets/Slot Machine/SlotMachineController.gd
@@ -19,11 +19,31 @@ func assignSlots() -> void:
 		if c is SlotMachine:
 			slots.append(c)
 	
+	self.currentSlot = 1
 	for i in range(1, slotAmount+1):
 		var slot: SlotMachine = slots.pick_random()
 		slots.erase(slot)
 		slot.assignNumber(i)
+		slot.SlotMachineActivated.connect(self.updateMemoryGame)
 		await get_tree().create_timer(.5).timeout
+	
+	for s in slots:
+		s.assignNumber(-1)
+
+func updateMemoryGame(order_number: int):
+	if order_number == self.currentSlot:
+		self.currentSlot = self.currentSlot + 1
+		self.checkWinCondition()
+	else:
+		self.resetWinCondition()
+		
+func checkWinCondition():
+	if self.currentSlot < self.slotAmount+1: return
+	$Item.visible = true
+	
+func resetWinCondition():
+	# TO-DO: Play sad sound
+	self.assignSlots()
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.get_parent() is not Player: return
