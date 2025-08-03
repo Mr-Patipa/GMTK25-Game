9ef2ceb6ba6e1bc8f0c9b5938d58c22d6deaf43e
Mr-Patipa <rata168168@gmail.com>
2025-08-02 20:16:14 -0600

    Scene Update and Management

--- Globals/SceneManager.gd
+++ Globals/SceneManager.gd
@@ -2,3 +2,4 @@ extends Node
 
 const LEVEL: String = "res://Scenes/PlayerGUI/Level/Level.tscn"
 const MAIN_MENU: String = "res://Scenes/PlayerGUI/MainMenu/MainMenu.tscn"
+const ROOT : String = "res://Scenes/root.tscn"
--- Scenes/PlayerGUI/MainMenu/MainMenu.gd
+++ Scenes/PlayerGUI/MainMenu/MainMenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file(SceneManager.LEVEL)
+	get_tree().change_scene_to_file(SceneManager.ROOT)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
